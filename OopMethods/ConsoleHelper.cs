618b9e182b76bada7672bb48b7e27a57e636795e
Pohuș Tabita <emanuela.pohus@emanuel.ro>
2023-11-16 18:34:02 -1200

    method overloading usage

---
+++ OopMethods/ConsoleHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopMethods
+{
+    internal class ConsoleHelper
+    {
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Print(string label, string message)
+        {
+            if (!string.IsNullOrEmpty(label))
+            {
+                Console.WriteLine($"{label}: ");
+            }
+
+            Console.WriteLine(message);
+        }
+
+        public static void Print(string message, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ OopMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OopMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHelper.Print("test");
+            ConsoleHelper.Print("label","test");
+            ConsoleHelper.Print("test",3);
+        }
+    }
+}
